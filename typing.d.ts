de1566600ae5003734d5b1c94f099e6141bff5d3
Sunny <ratsunny@gmail.com>
2016-11-22 14:30:16 +0800

    Typings: Fix typescript import

--- typing.d.ts
+++ typing.d.ts
@@ -1,9 +1,20 @@
 interface FtpOptions {
+  slot?: 'ftp'
   host: string
   user: string
   password: string
 }
 
-declare var uploader: (remoteFolder: string, ftpList: FtpOptions[]) => NodeJS.ReadWriteStream
+interface UPYUNOptions {
+  slot: 'upyun'
+  api_host: string
+  operator: string
+  bucket: string
+  password: string
+}
+
+type CDNOptions = FtpOptions | UPYUNOptions
+
+declare var uploader: (remoteFolder: string, cdnList: CDNOptions[]) => NodeJS.ReadWriteStream
 
 export = uploader
